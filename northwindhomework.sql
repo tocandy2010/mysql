7fa8aadc52f125bcc8a126ba5cf0fcbb20d5a26a
yijie <you@example.com>
2019-06-28 17:59:03 +0800

    sql作業

--- northwindhomework.sql
+++ northwindhomework.sql
@@ -101,3 +101,158 @@ create procedure yijie04(in score int)
     -> select 'E';
     -> end if;
     -> end; ##
+
+-- 自訂函數 case when
+BEGIN
+case
+when x>y then 
+select "Big";
+when x<y THEN
+select "small";
+ELSE 
+select "equal";
+end case;
+end
+
+BEGIN
+DECLARE result varchar(10) default '';
+case XA
+when 1 then set result = 'a';
+when 10 then set result = 'b';
+when 100 then set result = 'c';
+end case;
+return result;
+end;
+
+
+--while do迴圈
+BEGIN
+	declare i int default 1;
+    declare j int default 66 ;
+    while i<10 DO
+        set i = i + 1;
+    end while;
+END
+
+--REPEAT迴圈
+BEGIN
+	declare i , s int default 0;
+	REPEAT
+    set i = i+1;
+    set s = s+i;
+	until i >= x
+    end REPEAT;
+    return s;
+END
+
+-- 無窮迴圈
+BEGIN
+	declare i int default 0;
+	yijie:LOOP
+    	if i < 7 then
+			select 'hello world';
+         else
+            leave yijie;
+         end if;
+         set i = i+1;
+	end loop yijie;
+    select i;
+END
+
+
+BEGIN
+	declare i int default 0;
+	yijie:loop
+    	if i <7 THEN
+        	select 'hello word';
+        elseIF i>10 THEN
+        	leave yijie;
+        ELSE
+        	select 'restart';
+        	set i = 100;
+        	ITERATE yijie;
+         end IF;
+         set i = i+1;
+    end loop yijie;
+    select i;
+    end
+
+
+    --錯誤訊息處理
+    BEGIN
+
+	declare exit HANDLER for SQLSTATE '23000'
+        BEGIN
+			SELECT 'OOOP!';
+        END;
+	INSERT INTO students (cid,cname,csex,cbirthday) values
+	(id,name,sex,birthday);
+    select 'OK';
+END
+
+
+--mysql 箭頭指向
+BEGIN
+	declare i int;
+    declare vexit int default 0;
+    declare id int default 0;
+    declare name varchar(10) DEFAULT '';
+    declare curs cursor FOR
+    	select cid,cname from students;
+    open curs;
+    
+    fetch curs into id,name;
+    close curs;
+    
+    select concat(id,':',name);
+END
+
+--游標尋訪
+BEGIN
+    declare vexit int default 0;
+    declare id int default 0;
+    declare name varchar(10) DEFAULT '';
+    declare curs cursor FOR
+    	select cid,cname from students;
+    declare CONTINUE HANDLER FOR NOT FOUND set vexit  =1;
+    open curs;
+    yijie:loop
+        fetch curs into id,name;
+        if vexit = 1 then 
+            leave yijie;
+        end if;
+        select concat(id,':',name);
+    END LOOP yijie;
+    close curs;
+END
+
+
+--游標尋訪並更新
+BEGIN
+	declare flag int default 0;
+    declare id,scoree int default 0;
+    declare name varchar(10) default "";
+    declare newstatus varchar(10) default "";
+	declare curs cursor for 
+    select cid,cname,score from students;
+   	declare continue handler for not found set flag = 1;
+    
+    open curs;
+    
+    yijie: LOOP
+    	FETCH curs into id,name,scoree;
+        if flag = 1 THEN
+        	leave yijie;
+        end if;
+        if scoree >=60 
+        	then set newstatus = "及格";
+        else
+        	set newstatus  = '不及格';
+        end if;
+    	select concat(name,':',scoree);
+    	UPDATE students set status = newstatus where cid = id;
+    end loop yijie;
+    close curs;
+
+
+END
